e8822dcdb9e35f5424c11ba180060b502e607b90
vroyibg <vroyibg@gmail.com>
2016-08-08 11:15:26 +0700

    Add more tranfer functions

--- NeutralNetwork.py
+++ NeutralNetwork.py
@@ -3,6 +3,34 @@
 #
 import numpy as np
 
+
+# Transfer function
+def sgm(x, Derivative=False):
+    if not Derivative:
+        return 1.0 / (1.0 + np.exp(-x))
+    else:
+        out = sgm(x)
+        return out * (1.0 - out)
+
+def linear(x, Derivative=False):
+    if not Derivative:
+        return x
+    else:
+        return 1.0
+def gausian(x,Derivative = False):
+    if not Derivative:
+        return np.exp(-x**2)
+    else:
+        return -2*x*np.exp(-x**2)
+def tanh(x,Derivate = False):
+    if not Derivate:
+        return np.tanh(x)
+    else:
+        return 1.0 - np.tanh(x)**2
+#
+#Classes
+#
+
 class BackPropagationNetwork:
     """A back-propagation network"""
     #
@@ -15,12 +43,29 @@ class BackPropagationNetwork:
     #
     #Class methods
     #
-    def __init__(self,layerSize):
+    def __init__(self,layerSize,layerFunction = None):
         """Initialize the network"""
 
         #layer info
         self.layerCount=len(layerSize) -1
         self.shape = layerSize
+
+        if layerFunction is None:
+            lFuncs= []
+            for i in range(self.layerCount):
+                if i == self.layerCount + 1:
+                    lFuncs.append(linear)
+                else:
+                    lFuncs.append(sgm)
+        else:
+            if len(layerSize) != len(layerFunction):
+                raise ValueError("Incompatible list of tranfer functions.")
+            elif layerFunction[0] is not None:
+                raise ValueError("Input layer can not have a layer function")
+            else:
+                lFuncs=layerFunction[1:]
+
+        self.tFuncs=lFuncs
 
         #Data from the last run
         self._layerInput = []
@@ -53,7 +98,7 @@ class BackPropagationNetwork:
             else:
                 layerInput = self.weights[index].dot(np.vstack([self._layerOutput[-1],np.ones([1,lnCases])]))
             self._layerInput.append(layerInput)
-            self._layerOutput.append(self.sgm(layerInput))
+            self._layerOutput.append(self.tFuncs[index](layerInput))
         return self._layerOutput[-1].T
 
     #
@@ -76,11 +121,11 @@ class BackPropagationNetwork:
                 #Compare to the target values
                 output_delta = self._layerOutput[index] - target.T
                 error = np.sum(output_delta**2)
-                delta.append(output_delta * self.sgm(self._layerInput[index],True))
+                delta.append(output_delta * self.tFuncs[index](self._layerInput[index],True))
             else:
                 #Compare to the following layer's delta
                 delta_pullback = self.weights[index + 1].T.dot(delta[-1])
-                delta.append(delta_pullback[:-1,:]*self.sgm(self._layerInput[index],True))
+                delta.append(delta_pullback[:-1,:]*self.tFuncs[index](self._layerInput[index],True))
         #Compute weight deltas
         for index in range(self.layerCount):
             delta_index = self.layerCount - 1 - index
@@ -97,26 +142,15 @@ class BackPropagationNetwork:
             self._previousWeightDelta[index] = weightDelta
         return error
 
-    #Transfer function
-    def sgm(self,x,Derivative=False):
-        if not Derivative:
-            return 1.0/(1.0+np.exp(-x))
-        else:
-            out=self.sgm(x)
-            return out*(1-out)
-
-
 #
 #If run as a script, create test object
 #
 if __name__ == "__main__":
-    bpn = BackPropagationNetwork((2,2,1))
-    print(bpn.shape)
-    print(bpn.weights)
 
     lvInput = np.array([[0,0],[1,1],[0,1],[1,0]])
-    lvTarget = np.array([[0.05],[0.05],[0.95],[0.95]])
-
+    lvTarget = np.array([[0.00],[0.00],[1.00],[1.00]])
+    lFuncs = [None,gausian,tanh,linear]
+    bpn = BackPropagationNetwork((2,2, 2, 1),lFuncs)
     lnMax = 100000
     lnError = 0.00001
     for i in range(lnMax + 1):
@@ -129,4 +163,5 @@ if __name__ == "__main__":
 
     #Dislay output
     lvOutput = bpn.Run(lvInput)
-    print("Input : {0}\nOutput: {1}".format(lvInput,lvOutput))
+    for i in range(lvInput.shape[0]):
+        print("Input : {0}  Output : {1}".format(lvInput[i],lvOutput[i]) )
